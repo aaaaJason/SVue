aeb51595737b5ff374a6cc345f4617ac053d7c97
Jason <fanghu8051321@gmail.com>
2024-06-12 17:43:58 +0800

    update login

---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+// Login.vue
+
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username">
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    <p v-if="error" style="color: red;">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: ''
+    };
+  },
+  methods: {
+    login() {
+      // 在這裡進行簡單的登錄模擬
+      if (this.username === 'user' && this.password === '1234') {
+        // 登錄成功，跳轉到首頁或其他頁面
+        this.$router.push('/main');
+      } else {
+        // 登錄失敗，顯示錯誤信息
+        this.error = 'Invalid username or password';
+      }
+    }
+  }
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -5,3 +5,15 @@ module.exports = defineConfig({
     'vuetify'
   ]
 })
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        //target 我们要连接的后端地址
+        target: 'http://localhost:3000',
+        changeOrigin: true, //是否跨域
+        pathRewrite: { '^/api': '' }
+      },
+    },
+  },
+}
